aca379e83b7193380e308f29cbef44e11ba4bc0e
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-5.local>
2021-04-13 20:56:38 -0400

    Wrapper logic for matrix generators

--- server/src/main/java/edu/brown/cs/student/coreachord/CoreaApp/LowDiversityGenerator.java
+++ server/src/main/java/edu/brown/cs/student/coreachord/CoreaApp/LowDiversityGenerator.java
@@ -1,12 +1,25 @@
 package edu.brown.cs.student.coreachord.CoreaApp;
 
 public class LowDiversityGenerator implements MatrixGenerator {
-  public LowDiversityGenerator() {
+  private TransitionMatrix tmat;
+  private double[][] matrix2d;
+  private final int numqualities = CoreaApplication.Quality.values().length;
+  private final int numroots = CoreaApplication.Root.values().length;
 
+  public LowDiversityGenerator() {
+    tmat = new TransitionMatrix();
+    matrix2d = tmat.getTransitionMatrix();
   }
 
   @Override
   public TransitionMatrix generateMatrix() { // implement this method with logic, rules
+    for (int i = 0; i < numqualities * numroots; i++) { // row
+      double[] currrow = matrix2d[i]; // current row to fill
+      for (int j = 0; j < numqualities * numroots; i++) { // col
+        double currentry = matrix2d[i][j]; // current "entry" to fill
+        // some logic
+      }
+    }
     return null;
   }
 }
--- server/src/main/java/edu/brown/cs/student/coreachord/CoreaApp/MediumDiversityGenerator.java
+++ server/src/main/java/edu/brown/cs/student/coreachord/CoreaApp/MediumDiversityGenerator.java
@@ -1,12 +1,25 @@
 package edu.brown.cs.student.coreachord.CoreaApp;
 
 public class MediumDiversityGenerator implements MatrixGenerator {
-  public MediumDiversityGenerator() {
+  private TransitionMatrix tmat;
+  private double[][] matrix2d;
+  private final int numqualities = CoreaApplication.Quality.values().length;
+  private final int numroots = CoreaApplication.Root.values().length;
 
+  public MediumDiversityGenerator() {
+    tmat = new TransitionMatrix();
+    matrix2d = tmat.getTransitionMatrix();
   }
 
   @Override
   public TransitionMatrix generateMatrix() { // implement this method with logic, rules
+    for (int i = 0; i < numqualities * numroots; i++) { // row
+      double[] currrow = matrix2d[i]; // current row to fill
+      for (int j = 0; j < numqualities * numroots; i++) { // col
+        double currentry = matrix2d[i][j]; // current "entry" to fill
+        // some logic
+      }
+    }
     return null;
   }
 }
