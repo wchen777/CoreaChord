9c54ad8be92e0bfa7fb425232841e151889aa907
wchen77 <william_b_chen@brown.edu>
2021-04-16 22:47:56 -0400

    checkpoint

---
+++ server/src/main/java/edu/brown/cs/student/coreachord/Analyzer/ComplexityScore.java
@@ -0,0 +1,13 @@
+package edu.brown.cs.student.coreachord.Analyzer;
+
+public class ComplexityScore {
+
+  // x is the middle measure number in the measure window
+  private int x;
+
+  // the secret complexity score from 0 to 1, 0 being least complex and 1 being most complex
+  private double y;
+
+  
+
+}
---
+++ server/src/main/java/edu/brown/cs/student/coreachord/Analyzer/GeneratedCadence.java
@@ -0,0 +1,43 @@
+package edu.brown.cs.student.coreachord.Analyzer;
+
+import edu.brown.cs.student.coreachord.CoreaApp.CoreaApplication;
+
+public class GeneratedCadence {
+  private Cadence cadence;
+  private int start;
+  private int end;
+  private CoreaApplication.Root relativeRoot;
+
+  public GeneratedCadence (Cadence cadence, int start, int end, CoreaApplication.Root relativeRoot) {
+    this.cadence = cadence;
+    this.start = start;
+    this.end = end;
+    this.relativeRoot = relativeRoot;
+  }
+
+//  public int getEnd() {
+//    return end;
+//  }
+//
+//  public Cadence getCadence() {
+//    return cadence;
+//  }
+//
+//  public int getStart() {
+//    return start;
+//  }
+//
+//  public CoreaApplication.Root getRelativeRoot() {
+//    return relativeRoot;
+//  }
+
+  @Override
+  public String toString() {
+    return "GeneratedCadence{" +
+            "cadence=" + cadence +
+            ", start=" + start +
+            ", end=" + end +
+            ", relativeRoot=" + relativeRoot +
+            '}';
+  }
+}
